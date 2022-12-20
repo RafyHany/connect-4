d88c75df2efb6adec63650b7bb93d0ed274167f4
polahany <pola.hany.fayez@gmail.com>
2022-12-20 22:48:34 +0200

    main menu

--- connect4.c.c
+++ connect4.c.c
@@ -3,24 +3,193 @@
 #include <math.h>
 #include <string.h>
 #include <time.h>
+#include <windows.h>
 
+//main functions
+
+int main_menu(void);
 void time_passed(time_t start_time);
 int computer_turn(int width);
 
 
 int main()
 {
+    main_menu();
+   /* switch (main_menu())
+    {
+        case 0 : pvp();
+          break ;
+        case 1 : pvc()
+          break ;
+    }*/
     time_t start_time=time(NULL);
          srand(time(NULL));
 
 
     return 0;
 }
+
+
+// comp turn
+
+
 int computer_turn(int width)
 {
  int computer_choice=rand()%width+1;
  return computer_choice;
 }
+
+
+
+// the main menu
+
+void gotoxy(int x,int y)
+{
+    COORD coord = {0,0};
+    coord.X=x;
+    coord.Y=y;
+    SetConsoleCursorPosition(GetStdHandle(STD_OUTPUT_HANDLE),coord);
+}
+
+void draw_the_box(int p , int h , int f , int g)
+{
+    int i ;
+    gotoxy(f,g);
+    printf("%c",201);
+    for( i = 0 ; i < p ; i++)
+        printf("%c",205);
+    printf("%c",187);
+    gotoxy(f,g);
+    for(int j = 0 ; j < h-2 ; j++)
+    {
+        gotoxy(f,g+1+j);
+        printf("%c",186);
+        for( i = 0 ; i < p ; i++)
+           printf("%c",32);
+        printf("%c",186);
+    }
+
+    gotoxy(f,g+h-1);
+    printf("%c",200);
+    for( i = 0 ; i < p ; i++)
+        printf("%c",205);
+    printf("%c",188);
+}
+
+void ShowConsoleCursor(int showFlag){
+    HANDLE out = GetStdHandle(STD_OUTPUT_HANDLE);
+
+    CONSOLE_CURSOR_INFO cursorInfo;
+
+    GetConsoleCursorInfo(out, &cursorInfo);
+    cursorInfo.bVisible = showFlag;
+    SetConsoleCursorInfo(out, &cursorInfo);
+}
+
+
+int main_menu(void)
+{
+    // basic printing
+    ShowConsoleCursor(0);
+    draw_the_box(30,6,39,11);
+    int pos = 0 ;
+    char c ;
+    do{
+    HANDLE console_color = GetStdHandle(STD_OUTPUT_HANDLE);
+    SetConsoleTextAttribute(console_color , 15 );
+
+
+    gotoxy(40,12);printf(" 1) Start a new game          ");
+    gotoxy(40,13);printf(" 2) Load a game               ");
+    gotoxy(40,14);printf(" 3) Top players               ");
+    gotoxy(40,15);printf(" 4) Quit                      ");
+    printf("\n");
+
+
+        switch(pos)
+        {
+            case 0 : SetConsoleTextAttribute(console_color , 240 );
+                    gotoxy(40,12);printf(" 1) Start a new game          ");
+                    break ;
+            case 1 : SetConsoleTextAttribute(console_color , 240 );
+                    gotoxy(40,13);printf(" 2) Load a game               ");
+                    break ;
+            case 2 : SetConsoleTextAttribute(console_color , 240 );
+                    gotoxy(40,14);printf(" 3) Top players               ");
+                    break ;
+            case 3 : SetConsoleTextAttribute(console_color , 240 );
+                    gotoxy(40,15);printf(" 4)Quit                       ");
+                    break ;
+        }
+
+        c = getch();
+        if(c == 80)
+        {
+            pos = (++pos) % 4;
+        }
+        else if(c == 72)
+        {
+            pos = (--pos) % 4 ;
+        }
+    }while( c != 13);
+    printf("\n");
+
+
+    // after choosing
+    HANDLE console_color = GetStdHandle(STD_OUTPUT_HANDLE);
+    SetConsoleTextAttribute(console_color , 15 );
+
+    switch(pos)
+
+     {case 0 :   gotoxy(39,19);
+        draw_the_box(30,8,39,19);
+     do{
+    HANDLE console_color = GetStdHandle(STD_OUTPUT_HANDLE);
+    SetConsoleTextAttribute(console_color , 15 );
+
+
+    gotoxy(40,20);printf(" 1) Start a new game          ");
+    gotoxy(40,21);printf("   i) player vs player        ");
+    gotoxy(40,22);printf("   ii)player vs comp          ");
+    gotoxy(40,23);printf(" 2) Load a game               ");
+    gotoxy(40,24);printf(" 3) Top players               ");
+    gotoxy(40,25);printf(" 4) Quit                      ");
+    printf("\n");
+
+
+        switch(pos)
+        {
+            case 0 : SetConsoleTextAttribute(console_color , 240 );
+                    gotoxy(40,21);printf("   i) player vs player        ");
+                    break ;
+            case 1 : SetConsoleTextAttribute(console_color , 240 );
+                    gotoxy(40,22);printf("   ii)player vs comp          ");
+                    break ;
+        }
+
+        c = getch();
+        if(c == 80)
+        {
+            pos = (++pos) % 2;
+        }
+        else if(c == 72)
+        {
+            pos = (--pos) % 2 ;
+        }
+    }while( c != 13);
+    printf("\n\n\n\n");
+
+    return pos ;
+
+    break ;
+}
+
+}
+
+
+// print time passed
+
+
 void time_passed(time_t start_time)
 {
     time_t current_time;
